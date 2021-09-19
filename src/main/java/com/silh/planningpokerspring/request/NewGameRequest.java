b1562973ad5bf51ce4ee1b3007cd76044117aa0f
Ilia Beliaev <i.beliaev@activevideo.com>
2021-09-19 20:34:56 +0200

    Moved to Java 17.

--- src/main/java/com/silh/planningpokerspring/request/NewGameRequest.java
+++ src/main/java/com/silh/planningpokerspring/request/NewGameRequest.java
@@ -1,15 +1,6 @@
 package com.silh.planningpokerspring.request;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 
-@Data
-public class NewGameRequest {
-  private final String name;
-
-  @JsonCreator
-  public NewGameRequest(@JsonProperty("name") String name) {
-    this.name = name;
-  }
+public record NewGameRequest(@JsonProperty("name") String name) {
 }
--- src/main/java/com/silh/planningpokerspring/request/TransitionRequest.java
+++ src/main/java/com/silh/planningpokerspring/request/TransitionRequest.java
@@ -1,17 +1,7 @@
 package com.silh.planningpokerspring.request;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.silh.planningpokerspring.domain.GameState;
-import lombok.Data;
 
-@Data
-public class TransitionRequest {
-  private final GameState nextState;
-
-  @JsonCreator
-  public TransitionRequest(
-    @JsonProperty("nextState") GameState nextState) {
-    this.nextState = nextState;
-  }
+public record TransitionRequest(@JsonProperty("nextState") GameState nextState) {
 }
